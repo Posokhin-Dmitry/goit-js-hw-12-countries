4bae9695275edb314c15b76fdf39f7fafd196259
Dmitry Posokhin <79544402+NEGANN@users.noreply.github.com>
2021-09-26 21:19:18 +0300

    v2

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,4 @@
-const URL = 'https://restcountries.eu/rest/v2/name';
+const URL = 'https://restcountries.com/v2/name';
 
 function fetchCountry(searchQuery) {
   return fetch(`${URL}/${searchQuery}`).then(response => response.json());
